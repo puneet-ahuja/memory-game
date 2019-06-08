03ffea8af7d65c6e1576f9c801cbcb2cbac43da1
puneet-ahuja <ahujapuneet92@gmail.com>
2019-06-08 14:53:41 +0530

    Updates

---
+++ precache-manifest.2ac7b8335685e39ff1891846591178f6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb5d1320dfa067f24ec11484b4e85fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c906cc3cbc50b71b161",
+    "url": "/static/css/main.7e41c4b2.chunk.css"
+  },
+  {
+    "revision": "9a5bc4e9ca798b4abef4",
+    "url": "/static/js/2.5c56366d.chunk.js"
+  },
+  {
+    "revision": "6c906cc3cbc50b71b161",
+    "url": "/static/js/main.b29f68e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "84bcbfac8dd450d1f87c757666b46e80",
+    "url": "/static/media/apple.84bcbfac.svg"
+  },
+  {
+    "revision": "f0bf1d3efffd7ec12df389116fda9331",
+    "url": "/static/media/apple_dark.f0bf1d3e.svg"
+  },
+  {
+    "revision": "9b05bcfe8c559daf53a2d792f686fe7b",
+    "url": "/static/media/apple_empty.9b05bcfe.svg"
+  },
+  {
+    "revision": "0a6b561a9d99533b6b9e25843ccf3d25",
+    "url": "/static/media/lemon.0a6b561a.svg"
+  },
+  {
+    "revision": "16694f7f4c2bdae4cd1e3c4e7cb891d1",
+    "url": "/static/media/orange.16694f7f.svg"
+  },
+  {
+    "revision": "905c3947be9ee1ddc2e3bf8407a2cfc0",
+    "url": "/static/media/orange_dark.905c3947.svg"
+  },
+  {
+    "revision": "4e1142655ecb6025cf341794a360468b",
+    "url": "/static/media/watermelon.4e114265.svg"
+  },
+  {
+    "revision": "dd98a4bf45ee97f84007c6a48e659be9",
+    "url": "/static/media/watermelon_dark.dd98a4bf.svg"
+  }
+]);
