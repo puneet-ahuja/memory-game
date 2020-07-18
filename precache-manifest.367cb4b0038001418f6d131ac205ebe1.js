8f6041b600f820804b5803ebb17eb7ad19f8e282
Puneet Ahuja <puneet.ahuja@olx.com>
2020-07-18 13:36:58 +0530

    Updates

---
+++ precache-manifest.367cb4b0038001418f6d131ac205ebe1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a065b943ecef9313d00f502e9ed1cf",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "ed89e0131a92f673da57",
+    "url": "/memory-game/static/css/main.8fb578c4.chunk.css"
+  },
+  {
+    "revision": "9a5bc4e9ca798b4abef4",
+    "url": "/memory-game/static/js/2.5c56366d.chunk.js"
+  },
+  {
+    "revision": "ed89e0131a92f673da57",
+    "url": "/memory-game/static/js/main.a6e174e0.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  },
+  {
+    "revision": "84bcbfac8dd450d1f87c757666b46e80",
+    "url": "/memory-game/static/media/apple.84bcbfac.svg"
+  },
+  {
+    "revision": "f0bf1d3efffd7ec12df389116fda9331",
+    "url": "/memory-game/static/media/apple_dark.f0bf1d3e.svg"
+  },
+  {
+    "revision": "9b05bcfe8c559daf53a2d792f686fe7b",
+    "url": "/memory-game/static/media/apple_empty.9b05bcfe.svg"
+  },
+  {
+    "revision": "47729c53c19de2b5de89df711f85c208",
+    "url": "/memory-game/static/media/card_back.47729c53.jpg"
+  },
+  {
+    "revision": "0a6b561a9d99533b6b9e25843ccf3d25",
+    "url": "/memory-game/static/media/lemon.0a6b561a.svg"
+  },
+  {
+    "revision": "16694f7f4c2bdae4cd1e3c4e7cb891d1",
+    "url": "/memory-game/static/media/orange.16694f7f.svg"
+  },
+  {
+    "revision": "905c3947be9ee1ddc2e3bf8407a2cfc0",
+    "url": "/memory-game/static/media/orange_dark.905c3947.svg"
+  },
+  {
+    "revision": "4e1142655ecb6025cf341794a360468b",
+    "url": "/memory-game/static/media/watermelon.4e114265.svg"
+  },
+  {
+    "revision": "dd98a4bf45ee97f84007c6a48e659be9",
+    "url": "/memory-game/static/media/watermelon_dark.dd98a4bf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.12001a93a6872f868b844d2b649ab802.js"
+  "/memory-game/precache-manifest.367cb4b0038001418f6d131ac205ebe1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/memory-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
